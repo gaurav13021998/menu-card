ebc627fc471843aedde9d582efd272ea244e9ce7
Gaurav Singh <gaurav13021997@gmail.com>
2021-06-21 10:55:44 +0530

    Updates

---
+++ precache-manifest.8c94b791ce8df4e8ca3f11c9438f5d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d896db9d4b64830679ba0788c0668112",
+    "url": "/menu-card/index.html"
+  },
+  {
+    "revision": "95847e025bf1b48923db",
+    "url": "/menu-card/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu-card/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu-card/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95847e025bf1b48923db",
+    "url": "/menu-card/static/js/main.96b61c03.chunk.js"
+  },
+  {
+    "revision": "022ba7722e4f72ce1288",
+    "url": "/menu-card/static/js/runtime-main.04e7b717.js"
+  }
+]);
